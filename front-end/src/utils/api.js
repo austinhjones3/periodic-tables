db4aae2ffff12fcd8976afc61c291180e19a8ce3
Austin Jones` <austinhjones3@gmail.com>
2021-05-17 16:52:15 -0500

    whoops

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -70,7 +70,7 @@ export async function listReservations(params, signal) {
 export async function createReservation(reservation, signal) {
   const url = `${API_BASE_URL}/reservations`;
   reservation.people = Number(reservation.people);
-  return await fetch(url, {
+  return await fetchJson(url, {
     body: JSON.stringify({ data: reservation }),
     headers,
     method: "POST",
