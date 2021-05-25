09c73072f7b0690fdc164b39bb6449dfa7eb6693
austinhjones3 <austinhjones3@gmail.com>
2021-05-25 14:34:13 -0500

    still trying to make frontend tests pass

--- front-end/src/dashboard/TableCard.jsx
+++ front-end/src/dashboard/TableCard.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { deletePartyFromTable } from "../utils/api";
+import { useHistory } from "react-router-dom";
 
 export default function TableCard({ calledAPI, setCalledAPI, table }) {
   const [error, setError] = useState(null);
-
+  const history = useHistory();
   function handleFinish() {
     const abortController = new AbortController();
     const answer = window.confirm(
@@ -12,6 +13,7 @@ export default function TableCard({ calledAPI, setCalledAPI, table }) {
     if (answer) {
       deletePartyFromTable(table.table_id, abortController.signal)
         .then(() => setCalledAPI(() => !calledAPI))
+        .then(history.go(0))
         .catch(setError);
     }
   }
@@ -22,11 +24,11 @@ export default function TableCard({ calledAPI, setCalledAPI, table }) {
         <h5 className="card-title">Table: {table.table_name}</h5>
         <p className="card-text">Capacity: {table.capacity}</p>
         <p data-table-id-status={table.table_id}>
-          Status: {table.reservation_id ? "Occupied" : "Free"}
+          Status: {table.reservation_id !== null ? "Occupied" : "Free"}
         </p>
-        {table.reservation_id && (
+        {table.reservation_id !== null && (
           <button
-            type="button"
+            type="submit"
             className="btn btn-danger"
             onClick={handleFinish}
             data-table-id-finish={table.table_id}
